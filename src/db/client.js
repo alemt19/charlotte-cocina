a04c5f8cf93de4a2183ec9d9fb70df79df64f3c0
alemt19 <alfredomt1906@gmail.com>
2025-12-16 23:24:05 -0400

    fix: Revertir cambios en schema.prisma, client.js y arreglar error en inventory.service.js

--- src/db/client.js
+++ src/db/client.js
@@ -1,12 +1,16 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '../generated/prisma/index.js';
+import { PrismaPg } from '@prisma/adapter-pg';
+
+const connectionString = process.env.DATABASE_URL;
+const adapter = new PrismaPg({ connectionString });
 
 // Patrón Singleton para evitar múltiples conexiones en desarrollo (Hot Reload)
 const globalForPrisma = global;
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV !== 'production') {
     globalForPrisma.prisma = prisma;
 }
 
-export * from '@prisma/client';
+export * from '../generated/prisma/index.js';
