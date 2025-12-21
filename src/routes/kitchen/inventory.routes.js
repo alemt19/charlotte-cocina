31de621efa5aab3937341dc455c12f67fbf46c73
Leandert23 <toroleander23@gmail.com>
2025-12-21 12:36:24 -0400

    Endpoints 3 y 4

--- src/routes/kitchen/inventory.routes.js
+++ src/routes/kitchen/inventory.routes.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { listItems, createItem } from '../../controllers/kitchen/inventory.controller.js';
+import { listItems, createItem, getItem, updateItem } from '../../controllers/kitchen/inventory.controller.js';
 
 const router = express.Router();
 
 router.get('/items', listItems);
 router.post('/items', createItem);
+router.get('/items/:id', getItem);
+router.patch('/items/:id', updateItem);
 
 export default router;
