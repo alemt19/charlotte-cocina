26fe9c748b48a35a940f537a7af9cbb661d8c1aa
Leandert23 <toroleander23@gmail.com>
2025-12-28 20:48:08 -0400

    Proteger assets

--- src/routes/inventory/assets.routes.js
+++ src/routes/inventory/assets.routes.js
@@ -1,14 +1,15 @@
 import express from 'express';
+import { requirePermission } from '../../middlewares/security/permission.middleware.js';
 import { createAsset, listAssets, getAsset, updateAsset, getAssetLogs } from '../../controllers/inventory/asset.controller.js';
 import { registerAssetLog } from '../../controllers/inventory/asset.controller.js';
 
 const router = express.Router();
 
-router.get('/', listAssets);
-router.post('/', createAsset);
-router.get('/:id', getAsset);
-router.patch('/:id', updateAsset);
-router.post('/:id/logs', registerAssetLog);
-router.get('/:id/logs', getAssetLogs);
+router.get('/', requirePermission('KitchenAsset_cocina', 'Read'), listAssets);
+router.post('/', requirePermission('KitchenAsset_cocina', 'Create'), createAsset);
+router.get('/:id', requirePermission('KitchenAsset_cocina', 'Read'), getAsset);
+router.patch('/:id', requirePermission('KitchenAsset_cocina', 'Update'), updateAsset);
+router.post('/:id/logs', requirePermission('AssetLog_cocina', 'Create'), registerAssetLog);
+router.get('/:id/logs', requirePermission('AssetLog_cocina', 'Read'), getAssetLogs);
 
 export default router;
